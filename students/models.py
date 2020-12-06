0dd19b86b5def24a04c10f1152a982ac977c3d5d
Prakalpit <prakalpit.sangram@gmail.com>
2020-12-06 17:45:10 +0545

    Through views added Hello world in Home page. A .png image is added too.

--- students/models.py
+++ students/models.py
@@ -2,6 +2,6 @@ from django.db import models
 class Student(models.Model):
     name =models.CharField(max_length=27)
     Level=models.CharField(max_length=27)
-    fee  =models.FloatField(max_length=7, default='500')
+    fee  =models.FloatField(max_length=3, default='500')
     about=models.TextField(blank=True, null=True)
 # Create your models here.
