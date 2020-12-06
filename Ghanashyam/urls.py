016bf6b72d9c472dde207b0ebafb335b2d49df42
Prakalpit <prakalpit.sangram@gmail.com>
2020-12-06 18:07:23 +0545

    I am facing an error. ile "/Users/Prakalpit/Desktop/djangoproject/src/Ghanashyam/urls.py", line 22     path('', views.home, name='home'),     ^ SyntaxError: invalid syntax

--- Ghanashyam/urls.py
+++ Ghanashyam/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from students import views
 
 urlpatterns = [
-path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact')
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
--- students/views.py
+++ students/views.py
@@ -2,6 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(*args, **kwargs):
-    return HttpResponse("<h1>Hello world</h1>")
-
+    return HttpResponse("<h1>Hello world<br>"
+                        "Welcome to my webpage"
+                        "</h1>")
+def contact(*args, **kwargs):
+    return HttpResponse("<h1>You can contact me at drparishrameearticles@gmail.com<<br>"
+                        "or parishramee@debhiss.com/h1>")
 # Create your views here.
